cfec4317046d577a944e2141728801fa6c4cfc1e
Yonghao-Yu <75648505+Yonghao-Yu@users.noreply.github.com>
2024-09-23 20:21:05 +0800

    Update index.js
    顺序显示

--- static/index.js
+++ static/index.js
@@ -1,6 +1,18 @@
-// index.js
+// 监控的机器列表
+const MONITORED_MACHINES = [
+    "http://172.18.131.2:7788/status",
+    "http://172.18.131.4:7788/status",
+    "http://172.18.131.6:7788/status",
+    "http://172.18.131.8:7788/status",
+    "http://172.18.131.10:7788/status",
+    "http://172.18.131.12:7788/status",
+    "http://172.18.131.14:7788/status",
+    "http://172.18.131.16:7788/status",
+    "http://172.18.131.18:7788/status"
+];
+
 $(document).ready(function() {
-    let interval = 10000;  // 刷新间隔 30 秒
+    let interval = 10000;  // 刷新间隔 10 秒
 
     // 更新前端页面
     function updateContent(data) {
@@ -17,103 +29,105 @@ $(document).ready(function() {
         $('#statuses').empty();
         $('#errors').empty();
 
-        // 显示每台机器的状态
-        for (let url in data.statuses) {
+        // 按照 MONITORED_MACHINES 的顺序显示机器状态
+        MONITORED_MACHINES.forEach(function(url) {
             let machineData = data.statuses[url];
-            let machineDiv = `<h2>机器: ${url}</h2>`;
+            if (machineData) {
+                let machineDiv = `<h2>机器: ${url}</h2>`;
 
-            // GPU 信息表格
-            machineDiv += `
-                <h3>GPU 信息</h3>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>名称</th>
-                            <th>风扇速度 (%)</th>
-                            <th>GPU 温度 (°C)</th>
-                            <th>功耗 (W)</th>
-                            <th>功耗限制 (W)</th>
-                            <th>内存使用 (MB)</th>
-                            <th>内存总量 (MB)</th>
-                            <th>GPU 利用率 (%)</th>
-                            <th>PCI-E 宽度</th>
-                            <th>PCI-E 代数</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-            `;
-
-            machineData.gpu_info_list.forEach(function(gpu) {
+                // GPU 信息表格
                 machineDiv += `
-                    <tr>
-                        <td>${gpu.index}</td>
-                        <td>${gpu.name}</td>
-                        <td>${gpu.fan_speed}</td>
-                        <td>${gpu.temperature_gpu}</td>
-                        <td>${gpu.power_draw}</td>
-                        <td>${gpu.power_limit}</td>
-                        <td>${gpu.memory_used}</td>
-                        <td>${gpu.memory_total}</td>
-                        <td>${gpu.utilization_gpu}</td>
-                        <td>${gpu.pcie_width_current}</td>
-                        <td>${gpu.pcie_gen_current}</td>
-                    </tr>
+                    <h3>GPU 信息</h3>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>名称</th>
+                                <th>风扇速度 (%)</th>
+                                <th>GPU 温度 (°C)</th>
+                                <th>功耗 (W)</th>
+                                <th>功耗限制 (W)</th>
+                                <th>内存使用 (MB)</th>
+                                <th>内存总量 (MB)</th>
+                                <th>GPU 利用率 (%)</th>
+                                <th>PCI-E 宽度</th>
+                                <th>PCI-E 代数</th>
+                            </tr>
+                        </thead>
+                        <tbody>
                 `;
-            });
 
-            machineDiv += `
-                    </tbody>
-                </table>
-            `;
-
-            // 进程信息表格
-            machineDiv += `
-                <h3>进程信息</h3>
-                <table>
-                    <thead>
+                machineData.gpu_info_list.forEach(function(gpu) {
+                    machineDiv += `
                         <tr>
-                            <th>全局索引</th>
-                            <th>GPU ID</th>
-                            <th>容器名称</th>
-                            <th>进程启动时间</th>
-                            <th>进程运行时间</th>
-                            <th>PID (Host)</th>
-                            <th>PID (Container)</th>
-                            <th>进程名称</th>
-                            <th>GPU 内存使用 (MB)</th>
-                            <th>主内存使用 (MB)</th>
-                            <th>命令</th>
+                            <td>${gpu.index}</td>
+                            <td>${gpu.name}</td>
+                            <td>${gpu.fan_speed}</td>
+                            <td>${gpu.temperature_gpu}</td>
+                            <td>${gpu.power_draw}</td>
+                            <td>${gpu.power_limit}</td>
+                            <td>${gpu.memory_used}</td>
+                            <td>${gpu.memory_total}</td>
+                            <td>${gpu.utilization_gpu}</td>
+                            <td>${gpu.pcie_width_current}</td>
+                            <td>${gpu.pcie_gen_current}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-            `;
+                    `;
+                });
 
-            machineData.proc_info_list.forEach(function(proc) {
                 machineDiv += `
-                    <tr>
-                        <td>${proc.global_index}</td>
-                        <td>${proc.gpu_index}</td>
-                        <td>${proc.container_name}</td>
-                        <td>${proc.proc_start_time}</td>
-                        <td>${proc.proc_running_time}</td>
-                        <td>${proc.pid}</td>
-                        <td>${proc.pid_in_container}</td>
-                        <td>${proc.process_name}</td>
-                        <td>${proc.gpu_memory_used}</td>
-                        <td>${proc.main_memory_used}</td>
-                        <td>${proc.command}</td>
-                    </tr>
+                        </tbody>
+                    </table>
                 `;
-            });
 
-            machineDiv += `
-                    </tbody>
-                </table>
-            `;
+                // 进程信息表格
+                machineDiv += `
+                    <h3>进程信息</h3>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>全局索引</th>
+                                <th>GPU ID</th>
+                                <th>容器名称</th>
+                                <th>进程启动时间</th>
+                                <th>进程运行时间</th>
+                                <th>PID (Host)</th>
+                                <th>PID (Container)</th>
+                                <th>进程名称</th>
+                                <th>GPU 内存使用 (MB)</th>
+                                <th>主内存使用 (MB)</th>
+                                <th>命令</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                `;
 
-            $('#statuses').append(machineDiv);
-        }
+                machineData.proc_info_list.forEach(function(proc) {
+                    machineDiv += `
+                        <tr>
+                            <td>${proc.global_index}</td>
+                            <td>${proc.gpu_index}</td>
+                            <td>${proc.container_name}</td>
+                            <td>${proc.proc_start_time}</td>
+                            <td>${proc.proc_running_time}</td>
+                            <td>${proc.pid}</td>
+                            <td>${proc.pid_in_container}</td>
+                            <td>${proc.process_name}</td>
+                            <td>${proc.gpu_memory_used}</td>
+                            <td>${proc.main_memory_used}</td>
+                            <td>${proc.command}</td>
+                        </tr>
+                    `;
+                });
+
+                machineDiv += `
+                        </tbody>
+                    </table>
+                `;
+
+                $('#statuses').append(machineDiv);
+            }
+        });
 
         // 显示错误信息
         if (data.errors.length > 0) {
